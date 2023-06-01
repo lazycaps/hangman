60a8f5d402dfeaa428e9a7f8b0a60e767c837dfe
lazycaps <kaden301104@gmail.com>
2023-06-01 13:46:54 -0500

    guessing added

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import getpass
 
 guesses = 0
 wins = 0
-
 game_won = False
+
+
+# Python code to convert string to list character-wise
+def convert(string):
+    list1 = []
+    list1[:0] = string
+    return list1
+
 
 while True:
     word = getpass.getpass("Think of a word or press Q to quit: ").lower()
@@ -17,11 +24,14 @@ while True:
         continue
     else:
         break
+word_list = convert(word)
+blanks_list = convert(blanks)
+# Convert word to list
 
 remaining_word_length = len(word)
 
-while remaining_word_length != 0:
-    print(blanks)
+while ''.join(blanks_list) != ''.join(word_list):
+    print(''.join(blanks_list))
     guess = input("Guess a letter: ")
     if not str.isalpha(guess):
         print("Please use an alphabetic character.")
@@ -31,12 +41,22 @@ while remaining_word_length != 0:
 
     else:
         if word.lower().count(guess.lower()) >= 1:
-            guess_result = word.lower().count(guess.lower())
-            print("Yes! The letter", guess, "is in the word", guess_result, "times.")
-            remaining_word_length = remaining_word_length - guess_result
-            print(remaining_word_length)
+            guess_occurrences = word.lower().count(guess.lower())
+            indexes = [
+                index for index in range(len(word))
+                if word.startswith(guess, index)
+            ]
+            for idx, x in enumerate(blanks_list):
+                # print(idx, x)
+                if word_list[idx] == guess:
+                    blanks_list[idx] = guess
+            print("Yes! The letter", guess, "is in the word", guess_occurrences, "times.")
+            remaining_word_length = remaining_word_length - guess_occurrences
+            # print(remaining_word_length)
+            # print(indexes)
+            # Debug purposes only ^
+
         else:
             print("No, the letter", guess, "is not in the word.")
 
-    # word.count('guess')
 print("You guessed the word! Thanks for playing!")
